57102066a5c55422d4aeb4ce73f012066f3384d3
Murad <murad-arslangiraev-1994@mail.ru>
2020-12-06 20:51:10 +0300

    изменил init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql start
