23193a73909c9e00aad965d5e4cb7af397c33614
Murad <murad-arslangiraev-1994@mail.ru>
2020-12-06 21:14:01 +0300

    правка в файлы nginx и init

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart -b 0.0.0.0:8080 hello:app
+sudo /etc/init.d/gunicorn restart -b 0.0.0.0:8080 hello
