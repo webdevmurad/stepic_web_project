e8ac99513a072856785365c87652f1a6c53e7776
Murad <murad-arslangiraev-1994@mail.ru>
2020-12-07 11:14:16 +0300

    отрефакторил проект

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart 
+sudo /etc/init.d/mysql start
 gunicorn -b 0.0.0.0:8080 hello
