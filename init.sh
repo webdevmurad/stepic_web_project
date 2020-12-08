ee46b5a6471d7fd354158a1eb3dc66ec52a2a7ef
Murad <murad-arslangiraev-1994@mail.ru>
2020-12-08 21:39:12 +0300

    рефакторинг

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart 
 sudo /etc/init.d/mysql start
-gunicorn -b 0.0.0.0:8080 hello
+gunicorn -b 0.0.0.0:8080 hello:app
