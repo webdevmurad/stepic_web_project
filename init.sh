4e91c1647aa61313407d651ef42354bd029d03d2
Murad <murad-arslangiraev-1994@mail.ru>
2020-12-22 10:30:22 +0300

    исправил init

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn -b 0.0.0.0:8080 hello:app
+sudo /etc/init.d/mysql start
