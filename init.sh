ec02e767236740dc9cd26d502b2be20290db8171
Murad <murad-arslangiraev-1994@mail.ru>
2020-12-23 16:56:46 +0300

    изменения

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
+sudo pip3 install virtualenv
+sudo pip3 install pathlib2
+virtualenv -p python3 venv
+source venv/bin/activate
+pip3 install django
+
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
-sudo /etc/init.d/mysql start
-gunicorn --bind='http://127.0.0.1:8080' hello:application
+gunicorn -w 2 -c /home/box/web/etc/hello.py hello:application
