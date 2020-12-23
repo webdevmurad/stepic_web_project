622eb9713ef5306629684504fb1872f68c3a1a18
Murad <murad-arslangiraev-1994@mail.ru>
2020-12-23 15:27:29 +0300

    изменения

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
+gunicorn --bind='http://127.0.0.1:8080' hello:application
